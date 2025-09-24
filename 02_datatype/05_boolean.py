e87cdacfa15f586ae874b1431ff34b48eb05fbb3
유재혁 <yjk7454@naver.com>
2025-09-24 10:48:35 +0900

    feat: Sec02-05 논리 자료형 - 출력 예제 코드

---
+++ 02_datatype/05_boolean.py
@@ -0,0 +1,7 @@
+# boolean 자료형 - 참/거짓
+print(5 > 10) # 거짓 (5는 10보다 작다)
+print(5 > 10) # 참
+print(True) # python에서는 첫글자가 대문자
+print(False)
+print(not True) # 부정(!) - False
+print(not (5 > 10)) # 거짓 부정 - True
