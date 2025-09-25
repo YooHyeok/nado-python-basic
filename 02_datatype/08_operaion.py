2c68487a0449e66ee97b397bf41bb7a5b1cc0c34
유재혁 <yjk7454@naver.com>
2025-09-25 14:31:16 +0900

    feat: `Sec03-09 연산자` - 사칙연산, 몫, 나머지, 비교, 논리, 부정

---
+++ 02_datatype/08_operaion.py
@@ -0,0 +1,27 @@
+print(1+1) # 덧셈 연산 2
+print(3-2) # 뺄셈 연산 1
+print(5*2) # 곱셈 연산 10
+print(6/3) # 나눗셈 연산 2 (나눗셈의 경우 실수로 출력)
+
+print(2**3) # 곱셈2번은 제곱 - 2^3 = 8
+print(5%3) # 나머지 연산 - 2
+print(10%3) # 나머지 연산 - 1
+print(5//3) # 몫 연산 - 1
+print(10//3) # 몫 연산 - 3
+print(10>3) # 비교 연산 - True
+print(4>=7) # 비교 연산 - False
+print(10<3) # 비교 연산 - False
+print(5<=5) # 비교 연산 - True
+
+print(3==3) # 동등비교 - True
+print(4==2) # 동등비교 - False
+print(3+4 == 7) # 동등비교 - True
+print(1 != 3) # 동등 비교: 같지 않다: True
+print(not (1==3)) # 논리 부정 연산 - True
+print((3 > 0) and (3 < 5)) # and: 3은 0보다 크고, 5보다 작다 - True
+print((3 > 0) & (3 < 5)) # and는 &로 표기 가능
+print((3 > 0) or (3 > 5)) # or: 둘중 하나라도 true면 true
+print((3 > 0) | (3 > 5)) # or는 |로 표기 가능
+print(5 > 4 > 3) # 비교연산 연속 가능
+print(5 > 4 <6) # 비교연산 연속 가능
+
