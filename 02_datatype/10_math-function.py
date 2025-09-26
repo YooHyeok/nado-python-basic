5af2165e189629393f8952a9c7c265698b9ecfea
YooHyeokSchool <webdevyoo@gmail.com>
2025-09-26 09:58:19 +0900

    feat: `Sec03-11 숫자처리함수` - 기본 내장함수 및 math 라이브러리 예제

---
+++ 02_datatype/10_math-function.py
@@ -0,0 +1,12 @@
+print(abs(-5)) # abs: 절대값 - 5
+print(pow(4, 2)) # pow: 제곱 - 16 (4^2 -> 4*4)
+print(max(5,12)) # max: 최대값 - 12
+print(min(5,12)) # min: 최소값 - 5
+print(round(3.14)) # round: 반올림 - 3
+print(round(4.99)) # round: 반올림 - 5
+
+# math 라이브러리 -  import *을 통해 math 라이브러리로 부터 모든것을 불러옴
+from math import *
+print(floor(4.99)) # floor: 내림 - 4
+print(ceil(3.14)) # ceil: 올림 - 4
+print(sqrt(16)) # sqrt: 제곱근 - 4
