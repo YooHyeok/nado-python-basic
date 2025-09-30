f2ff0b7afbc32f7c2c040d6520062780fdaaf30b
유재혁 <yjk7454@naver.com>
2025-10-01 08:09:33 +0900

    feat: `Sec04_14` - 일반 문자열과 멀티라인 문자열

---
+++ 04_string/14_string.py
@@ -0,0 +1,13 @@
+sentence = '나는 소년입니다.'
+print(sentence)
+sentence2 = "파이썬은 쉬워요."
+print(sentence2)
+sentence3 = """
+나는 소년이고,
+파이썬은 쉬워요.
+""" # 멀티라인 문자열("""쌍) 
+sentence4 = '''
+피이썬은 쉽고,
+나는 소년입니다.
+''' # 멀티라인 문자열('''홑)
+print(sentence4)
