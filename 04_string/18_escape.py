f44025d29df89291f1038b9fe89b6d2754c65e6e
유재혁 <yjk7454@naver.com>
2025-10-02 09:05:47 +0900

    feat: `Sec04_18` - 탈출문자 `\`

---
+++ 04_string/18_escape.py
@@ -0,0 +1,12 @@
+print("백문이 불여일견\n백견이 불여일타") # 줄바꿈 \n
+
+print("저는 \"나도코딩\"입니다.") # \"\' 문장내에서 따옴표 사용 : "x"\"나도코딩\"x" = x"나도코딩"x
+print('저는 \'나도코딩\'입니다.')
+
+print("C:\\Users") # \\ : 문장내에서 \ 사용: \는 탈출문자이기 때문에 두개로 표현해줘야한다.
+
+print("Red Apple\rPine") # \r: 커서를 맨 앞으로 이동 - Red Apple\rPine = PineApple (커서를 이동 후 기존 문자 위에 삽입)
+
+print("Redd\bApple") # \b: 백스페이스(위치로부터 앞의 한 글자 삭제)
+
+print("Red\tApple") #\t : 탭(4칸)
