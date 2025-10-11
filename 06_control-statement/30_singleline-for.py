938e19931d0c871b0e02f7d36e777fbc88b31407
YooHyeok <yjk7454@naver.com>
2025-10-12 00:46:34 +0900

    feat: `Sec06_30` - 한줄 for문 개념 및 예제 (튜플로 바로 반환)

---
+++ 06_control-statement/30_singleline-for.py
@@ -0,0 +1,17 @@
+# 출석번호가 1 2 3 4 5 앞에 100을 붙이기로 함 -> 101, 102, 103, 104, 105
+students = [1, 2, 3, 4, 5]
+print(students)
+students = [i + 100 for i in students]
+print(students)
+
+# 학생 이름을 길이로 변환
+students = ["Iron man", "Thor", "I am groot"]
+print(students)
+students = [len(i) for i in students]
+print(students)
+
+# 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+print(students)
+students = [i.upper() for i in students]
+print(students)
