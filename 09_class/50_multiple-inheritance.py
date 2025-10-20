dacf5721c47597544cfd16b580e9b4973ef341d3
유재혁 <yjk7454@naver.com>
2025-10-20 19:38:04 +0900

    feat: `Sec09_50 클래스` - 다중 상속 개념 및 기본 예제(스타크래프트)

---
+++ 09_class/50_multiple-inheritance.py
@@ -0,0 +1,57 @@
+""" 
+[다중 상속]
+여러 클래스를 상속 받는것을 의미
+
+공중유닛 : 일반 유닉의 특성인 이름과 체력을 가지면서 날수있는 기능을 가진다.
+- 공격하는 유닛: 레이스
+- 공격하지 않는 유닛: 드랍십
+"""
+
+class Unit:
+  def __init__(self, name, hp):
+    self.name = name
+    self.hp = hp
+
+class AttackUnit(Unit):
+  def __init__(self, name, hp, damage):
+    Unit.__init__(self, name, hp)
+    self.damage = damage
+  
+  def attack(self, location):
+    print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(self.name, location, self.damage))
+
+  def damaged(self, damage):
+    print("{0} : {1} 데미지를 입었습니다.".format(self.name, self.damage))
+    self.hp -= damage
+    print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+    if self.hp <= 0:
+      print("{0} : 파괴 되었습니다.".format(self.name))
+
+  
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+  def __init__(self, flying_speed):
+    self.flying_speed = flying_speed
+
+  def fly(self, name, location):
+    print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
+    
+# 공중 유닛 클래스
+class FlyableUnit(Unit, Flyable):
+  def __init__(self, name, hp, flying_speed):
+    Unit.__init__(self, name, hp)
+    Flyable.__init__(self, flying_speed)
+
+# 드랍쉽: 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 x
+dropship = FlyableUnit("드랍쉽", 200, 5)
+dropship.fly(dropship.name, "2시")
+
+# 공중 공격 유닛 클래스 - 다중상속은, 로 구분
+class FlyableAttackUnit(AttackUnit, Flyable):
+  def __init__(self, name, hp, damage, flying_speed):
+    AttackUnit.__init__(self, name, hp, damage)
+    Flyable.__init__(self, flying_speed)
+
+# 발키리: 공중 공격 유닛, 한번에 14발 미사일 발사.
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
